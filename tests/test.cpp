9762dd39bfac55d16fcf4a213a26154c21f71a74
Кирилл <zoolohol@mail.ru>
2020-05-31 09:26:50 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,11 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include <builder.hpp>	
+
+int main(int argc, char* argv[]) {	
+    build(argc, argv);	
+}
 
 TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
